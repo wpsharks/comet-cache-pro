ff865ec1b720232d3ab5420b9256726002eefe75
JasWSInc <jason@websharks-inc.com>
2013-12-24 08:54:06 -0900

    Improving Multisite support; wipe on option updates/restores.

--- quick-cache-pro/includes/actions.php
+++ quick-cache-pro/includes/actions.php
@@ -149,7 +149,7 @@ namespace quick_cache // Root namespace.
 					$redirect_to = self_admin_url('/admin.php'); // Redirect preparations.
 					$query_args  = array('page' => __NAMESPACE__, __NAMESPACE__.'__updated' => '1');
 
-					plugin()->auto_clear_cache(); // May produce a notice.
+					plugin()->auto_wipe_cache(); // May produce a notice.
 
 					if(plugin()->options['enable']) // Enable.
 						{
@@ -192,7 +192,7 @@ namespace quick_cache // Root namespace.
 					$redirect_to = self_admin_url('/admin.php'); // Redirect preparations.
 					$query_args  = array('page' => __NAMESPACE__, __NAMESPACE__.'__restored' => '1');
 
-					plugin()->auto_clear_cache(); // May produce a notice.
+					plugin()->auto_wipe_cache(); // May produce a notice.
 
 					if(plugin()->options['enable']) // Enable.
 						{
