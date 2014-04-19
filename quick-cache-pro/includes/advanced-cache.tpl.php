e1aa79716a5aa6fae04dd0ce758451e8e10b95ac
JasWSInc <jason@websharks-inc.com>
2014-04-18 16:04:59 -0800

    Locking output buffers.

--- quick-cache-pro/includes/advanced-cache.tpl.php
+++ quick-cache-pro/includes/advanced-cache.tpl.php
@@ -231,7 +231,7 @@ namespace quick_cache // Root namespace.
 								}
 							exit($cache); // Exit with cache contents.
 						}
-					else ob_start(array($this, 'output_buffer_callback_handler')); // Start output buffering.
+					else ob_start(array($this, 'output_buffer_callback_handler', 0, 0)); // Start locked output buffering.
 				}
 
 			public function maybe_invalidate_when_logged_in_postload()
@@ -287,7 +287,7 @@ namespace quick_cache // Root namespace.
 								}
 							exit($cache); // Exit with cache contents.
 						}
-					else ob_start(array($this, 'output_buffer_callback_handler')); // Start output buffering.
+					else ob_start(array($this, 'output_buffer_callback_handler', 0, 0)); // Start locked output buffering.
 				}
 
 			public function output_buffer_callback_handler($buffer)
