01ba677ae9eba2731883e179798bcee22b395474
JasWSInc <jason@websharks-inc.com>
2014-04-24 01:08:54 -0800

    Continuing work on docBlocks.

--- quick-cache-pro/includes/advanced-cache.tpl.php
+++ quick-cache-pro/includes/advanced-cache.tpl.php
@@ -1157,22 +1157,25 @@ namespace quick_cache
 				}
 
 			/**
-			 * @raamdev This turns off `E_NOTICE` level errors in the shutdown phase to prevent the WP core function
-			 *    {@link wp_ob_end_flush_all()} from triggering `WP_DEBUG` notices in the shutdown phase.
+			 * This turns off `E_NOTICE` level errors in the shutdown phase to prevent the WP core function
+			 *    {@link \wp_ob_end_flush_all()} from triggering `WP_DEBUG` notices.
 			 *
 			 *    Ideally we would NOT do this. However, there is little choice.
 			 *    This is what makes it possible for Quick Cache to use a locked output buffer.
 			 *    e.g. `ob_start(handler, 0, 0)`. Having a locked output buffer makes Quick Cache more dependable.
+			 *
 			 *    In cases where there are theme/plugin conflicts, more of these should be reported to us now; i.e. site owners may
 			 *    report `WP_DEBUG` notices regarding failed attempts to close/clean/flush an out-of-order output handler.
-			 *    These can be caused by a rogue theme/plugin that is doing things w/ buffers that it should not be doing.
+			 *    These can be caused by a rogue theme/plugin that is doing things w/ buffers that it should not be.
 			 *
 			 *    The disabling of `E_NOTICE` here only impacts the PHP shutdown phase.
-			 *    i.e. Anything registered in PHP via {@link register_shutdown_function()}.
-			 *    e.g. {@link wp_ob_end_flush_all()} runs in the shutdown phase.
+			 *    i.e. Anything registered in PHP via {@link \register_shutdown_function()}.
+			 *    e.g. {@link \wp_ob_end_flush_all()} runs in the shutdown phase.
 			 *
 			 *    This will NOT prevent `E_NOTICE` level errors from being triggered when a
 			 *    theme/plugin does something out-of-order with an output buffer. Want WANT to see those!
+			 *
+			 * @since 140422 First documented version.
 			 *
 			 * @IMPORTANT I suspect we will see some reports of this suddenly showing up on sites where a conflict currently exists.
 			 *    Of course, that will be a GOOD thing in most cases, because QC will have not been working properly for sites with conflicts anyway.
@@ -1186,17 +1189,22 @@ namespace quick_cache
 					error_reporting(error_reporting() & ~E_NOTICE);
 				}
 
-			/*
-			 * @raamdev Now that we have expanded the functionality of this output handler,
-			 *    it is EXTREMELY important to remember that PHP may have already destructed internal
-			 *    object references by the time this is called upon.
+			/**
+			 * Output buffer handler; i.e. the cache file generator.
 			 *
 			 * @note We CANNOT depend on any WP functionality here; it will cause problems.
 			 *    Anything we need from WP should be saved in the postload phase as a scalar value.
 			 *
-			 * @note I optimized this routine a bit here and there.
-			 * @note I reorganized this method a bit here and there.
-			 * @note I added debug info message codes to this routine.
+			 * @since 140422 First documented version.
+			 *
+			 * @param string  $buffer The buffer from {@link \ob_start()}.
+			 * @param integer $phase A set of bitmask flags.
+			 *
+			 * @return string|boolean The output buffer, or `FALSE` to indicate no change.
+			 *
+			 * @throws \exception If unable to handle output buffering for any reason.
+			 *
+			 * @attaches-to {@link \ob_start()}
 			 */
 			public function output_buffer_callback_handler($buffer, $phase)
 				{
@@ -1296,7 +1304,18 @@ namespace quick_cache
 					throw new \exception(sprintf(__('Quick Cache: failed to write cache file for: `%1$s`; possible permissions issue (or race condition), please check your cache directory: `%2$s`.', $this->text_domain), $_SERVER['REQUEST_URI'], QUICK_CACHE_DIR));
 				}
 
-			public function maybe_compress_html($cache) // <https://github.com/WebSharks/HTML-Compressor>
+			/**
+			 * Runs HTML Compressor (if applicable).
+			 *
+			 * @since 140422 First documented version.
+			 *
+			 * @param string $cache Input cache file we want to compress.
+			 *
+			 * @return string The `$cache` with HTML compression applied (if applicable).
+			 *
+			 * @see https://github.com/WebSharks/HTML-Compressor
+			 */
+			public function maybe_compress_html($cache)
 				{
 					if(!$this->site_url) return $cache; // Not possible.
 					if(!QUICK_CACHE_HTMLC_ENABLE || !$this->plugin_file)
@@ -1338,9 +1357,15 @@ namespace quick_cache
 				}
 
 			/**
-			 * @raamdev This adds no-cache debugging info to the bottom of HTML/XML docs.
-			 *    These messages are all related to scenarios in which Quick Cache does NOT cache a particular page.
-			 *    Having this info in the source code (when debugging is enabled) should prove VERY useful.
+			 * Appends `NC_DEBUG_` info (if applicable).
+			 *
+			 * @since 140422 First documented version.
+			 *
+			 * @param string $doc Input string to append debug info to.
+			 * @param string $reason_code One of the `NC_DEBUG_` constants.
+			 * @param string $reason Optional; to override the default description with a custom message.
+			 *
+			 * @return string The `$doc` with debug info appended (if applicable).
 			 */
 			public function maybe_add_nc_debug_info($doc = NULL, $reason_code = '', $reason = '')
 				{
@@ -1476,32 +1501,9 @@ namespace quick_cache
 					return $doc."\n".'<!-- '.htmlspecialchars(sprintf(__('Quick Cache is NOT caching this page, %1$s', $this->text_domain), $reason)).' -->';
 				}
 
-			/**
-			 * Recursive directory iterator based on a regex pattern.
-			 *
-			 * @since 140422 First documented version.
-			 *
-			 * @param string $dir An absolute server directory path.
-			 * @param string $regex A regex pattern; compares to each full file path.
-			 *
-			 * @return \RegexIterator Navigable with {@link \foreach()}; where each item
-			 *    is a {@link \RecursiveDirectoryIterator}.
-			 */
-			public function dir_regex_iteration($dir, $regex)
-				{
-					$dir_iterator      = new \RecursiveDirectoryIterator($dir, \FilesystemIterator::KEY_AS_PATHNAME | \FilesystemIterator::CURRENT_AS_SELF | \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::UNIX_PATHS);
-					$iterator_iterator = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::CHILD_FIRST);
-					$regex_iterator    = new \RegexIterator($iterator_iterator, $regex, \RegexIterator::MATCH, \RegexIterator::USE_KEY);
-
-					return $regex_iterator;
-				}
-
 			/*
-			 * See also: `quick-cache-pro.inc.php` duplicate.
-			 * NOTE: the call to `is_ssl()` in this duplicate uses `$this->is_ssl()` because `is_ssl()`
-			 *    may NOT be available in this routine; i.e. it's not been loaded up yet.
-			 *
-			 * @TODO Find a way to centralize this section so it can be shared between both classes easily.
+			 * See also: `quick-cache-pro.inc.php` duplicates.
+			 *    @TODO Find a way to centralize this section so it can be shared between both classes easily.
 			 */
 
 			/**
@@ -1726,6 +1728,16 @@ namespace quick_cache
 					return ($tokens[$dashify] = $token_value);
 				}
 
+			/**
+			 * Produces a token based on the current user.
+			 *
+			 * @since 140422 First documented version.
+			 *
+			 * @return string Produces a token based on the current user;
+			 *    else an empty string if that's not possible to do.
+			 *
+			 * @note The return value of this function is cached to reduce overhead on repeat calls.
+			 */
 			public function user_token()
 				{
 					static $token; // Cache.
@@ -1743,6 +1755,35 @@ namespace quick_cache
 					return ($token = '');
 				}
 
+			/**
+			 * Recursive directory iterator based on a regex pattern.
+			 *
+			 * @since 140422 First documented version.
+			 *
+			 * @param string $dir An absolute server directory path.
+			 * @param string $regex A regex pattern; compares to each full file path.
+			 *
+			 * @return \RegexIterator Navigable with {@link \foreach()}; where each item
+			 *    is a {@link \RecursiveDirectoryIterator}.
+			 */
+			public function dir_regex_iteration($dir, $regex)
+				{
+					$dir_iterator      = new \RecursiveDirectoryIterator($dir, \FilesystemIterator::KEY_AS_PATHNAME | \FilesystemIterator::CURRENT_AS_SELF | \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::UNIX_PATHS);
+					$iterator_iterator = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::CHILD_FIRST);
+					$regex_iterator    = new \RegexIterator($iterator_iterator, $regex, \RegexIterator::MATCH, \RegexIterator::USE_KEY);
+
+					return $regex_iterator;
+				}
+
+			/**
+			 * Is the current request method `POST|PUT|DELETE`?
+			 *
+			 * @since 140422 First documented version.
+			 *
+			 * @return boolean `TRUE` if yes; else `FALSE`.
+			 *
+			 * @note The return value of this function is cached to reduce overhead on repeat calls.
+			 */
 			public function is_post_put_del_request()
 				{
 					static $is; // Cache.
