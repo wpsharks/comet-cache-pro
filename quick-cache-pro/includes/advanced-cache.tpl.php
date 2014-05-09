5496c4aaec62daa4c361e472e2d381a74ca652a3
JasWSInc <jason@websharks-inc.com>
2014-05-09 06:35:22 -0800

    Continuing work on base directory structure. https://github.com/WebSharks/Quick-Cache/issues/147

--- quick-cache-pro/includes/advanced-cache.tpl.php
+++ quick-cache-pro/includes/advanced-cache.tpl.php
@@ -1353,7 +1353,7 @@ namespace quick_cache
 						'cache_expiration_time'          => QUICK_CACHE_HTMLC_CACHE_EXPIRATION_TIME,
 						'cache_dir_public'               => QUICK_CACHE_HTMLC_CACHE_DIR_PUBLIC.$host_dir_token,
 						'cache_dir_url_public'           => $this->site_url.'/'.str_replace(ABSPATH, '', QUICK_CACHE_HTMLC_CACHE_DIR_PUBLIC.$host_dir_token),
-						'cache_dir_private'              => QUICK_CACHE_HTMLC_CACHE_DIR_PRIVATE,
+						'cache_dir_private'              => QUICK_CACHE_HTMLC_CACHE_DIR_PRIVATE.$host_dir_token,
 
 						'compress_combine_head_body_css' => QUICK_CACHE_HTMLC_COMPRESS_COMBINE_HEAD_BODY_CSS,
 						'compress_combine_head_js'       => QUICK_CACHE_HTMLC_COMPRESS_COMBINE_HEAD_JS,
@@ -1376,8 +1376,8 @@ namespace quick_cache
 			 * @since 140422 First documented version.
 			 *
 			 * @param null|string $doc Input string to append debug info to.
-			 * @param string $reason_code One of the `NC_DEBUG_` constants.
-			 * @param string $reason Optional; to override the default description with a custom message.
+			 * @param string      $reason_code One of the `NC_DEBUG_` constants.
+			 * @param string      $reason Optional; to override the default description with a custom message.
 			 *
 			 * @return string The `$doc` with debug info appended (if applicable).
 			 */
