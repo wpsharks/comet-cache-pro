fe44510c45e068144287dd7818b0f4c69016f6b6
JasWSInc <jas@wsharks.com>
2014-09-09 14:08:59 -0800

    Adding `cdn_base` class. Just getting started on websharks/quick-cache#55

---
+++ quick-cache-pro/includes/cdn-base.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * CDN Base Class.
+ *
+ * @package quick_cache\cdn
+ * @since 14xxxx Adding CDN support.
+ * @copyright WebSharks, Inc. <http://www.websharks-inc.com>
+ * @license GNU General Public License, version 2
+ */
+namespace quick_cache // Root namespace.
+{
+	if(!defined('WPINC')) // MUST have WordPress.
+		exit('Do NOT access this file directly: '.basename(__FILE__));
+
+	/**
+	 * CDN Base Class.
+	 */
+	class cdn_base
+	{
+		/**
+		 * @var plugin Quick Cache instance.
+		 */
+		protected $plugin; // Set by constructor.
+
+		/**
+		 * Class constructor.
+		 */
+		public function __construct()
+		{
+			$this->plugin = plugin();
+		}
+	}
+}
