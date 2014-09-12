27f6cb4dd56ac16521ddd522e038ef8bfba28e1e
JasWSInc <jas@wsharks.com>
2014-09-11 23:04:07 -0800

    Remove `vendor/`. See websharks/quick-cache#55

--- quick-cache-pro/quick-cache-pro.inc.php
+++ quick-cache-pro/quick-cache-pro.inc.php
@@ -12,7 +12,6 @@ namespace quick_cache
 	if(!defined('WPINC')) // MUST have WordPress.
 		exit('Do NOT access this file directly: '.basename(__FILE__));
 
-	require_once dirname(__FILE__).'/vendor/autoload.php';
 	require_once dirname(__FILE__).'/includes/share.php';
 
 	if(!class_exists('\\'.__NAMESPACE__.'\\plugin'))
