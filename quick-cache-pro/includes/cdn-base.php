6403f5fafca7e8b10544caa52506771f8b6db34f
JasWSInc <jas@wsharks.com>
2014-09-21 04:57:20 -0800

    Prepare URL filter-related methods. See: websharks/quick-cache#55

--- quick-cache-pro/includes/cdn-base.php
+++ quick-cache-pro/includes/cdn-base.php
@@ -23,11 +23,33 @@ namespace quick_cache // Root namespace.
 		protected $plugin; // Set by constructor.
 
 		/**
+		 * @var string CDN is for this host name.
+		 */
+		protected $host; // Set by constructor.
+
+		/**
+		 * @var string CDN serves files from this host.
+		 */
+		protected $cdn_host; // Set by constructor.
+
+		/**
+		 * @var array Array of CDN extensions.
+		 */
+		protected $cdn_extensions; // Set by constructor.
+
+		/**
 		 * Class constructor.
 		 */
 		public function __construct()
 		{
 			$this->plugin = plugin();
+
+			$this->host     = strtolower((string)parse_url(network_home_url(), PHP_URL_HOST));
+			$this->cdn_host = strtolower($this->plugin->options['cdn_host']);
+
+			$this->cdn_extensions = trim(strtolower($this->plugin->options['cdn_extensions']), "\r\n\t\0\x0B".' ;,');
+			$this->cdn_extensions = preg_split('/[|;,\s]+/', $this->cdn_extensions, NULL, PREG_SPLIT_NO_EMPTY);
+			$this->cdn_extensions = array_unique($this->cdn_extensions);
 		}
 
 		/**
@@ -52,14 +74,23 @@ namespace quick_cache // Root namespace.
 		{
 		}
 
-		/** @TODO Filter URLs that should be served by the CDN.
+		/**
+		 * Filter URLs that should be served by the CDN.
 		 *
-		 * @param string $url
+		 * @param string      $url Input URL passed by one of several filters.
+		 * @param string|null $scheme Scheme passed by one of several filters.
 		 *
 		 * @return string The URL after having been filtered.
 		 */
-		public function filter_url($url)
+		public function filter_url($url, $scheme = NULL)
 		{
+			if(!($local_file = $this->local_file($url)))
+				return $url; // Not local.
+
+			if(!in_array($local_file->extension, $this->cdn_extensions, TRUE))
+				return $url; // Not in the list of CDN extensions.
+
+			return set_url_scheme('//'.$this->cdn_host.$local_file->uri, $scheme);
 		}
 
 		/** @TODO Filter content for URLs that should be served by the CDN.
@@ -70,6 +101,42 @@ namespace quick_cache // Root namespace.
 		 */
 		public function filter_content($string)
 		{
+		}
+
+		protected function local_file($url_uri_query)
+		{
+			if(!$this->host) // Missing host name?
+				return NULL; // Not possible.
+
+			if(!($url_uri_query = trim((string)$url_uri_query)))
+				return NULL; // Unparseable.
+
+			if(!($parsed = @parse_url($url_uri_query)))
+				return NULL; // Unparseable.
+
+			if(!empty($parsed['host']) && strcasecmp($parsed['host'], $this->host) !== 0)
+				return NULL; // Not on this host name.
+
+			if(!isset($parsed['path'][0]) || substr($parsed['path'], -1) === '/')
+				return NULL; // No path. Or, not a file; i.e. a directory.
+
+			if(!($extension = $this->extension($parsed['path'])))
+				return NULL; // No extension; i.e. not a file.
+
+			$uri = $parsed['path']; // Put URI together.
+			if(strpos($uri, '/') !== 0) $uri = '/'.$uri;
+			if(!empty($parsed['query'])) $uri .= '?'.$parsed['query'];
+			if(!empty($parsed['fragment'])) $uri .= '#'.$parsed['fragment'];
+
+			return (object)compact('uri', 'extension');
+		}
+
+		protected function extension($path)
+		{
+			if(!($path = (string)$path))
+				return ''; // No path.
+
+			return strtolower(ltrim((string)strrchr(basename($path), '.'), '.'));
 		}
 	}
 }
