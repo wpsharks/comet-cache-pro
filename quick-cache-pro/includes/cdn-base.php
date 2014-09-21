779200279c99eb95c97131d57999c77bc4785c78
JasWSInc <jas@wsharks.com>
2014-09-21 06:09:10 -0800

    Continuing work on `content_filter()`. See: websharks/quick-cache#55

--- quick-cache-pro/includes/cdn-base.php
+++ quick-cache-pro/includes/cdn-base.php
@@ -50,6 +50,11 @@ namespace quick_cache // Root namespace.
 			$this->cdn_extensions = trim(strtolower($this->plugin->options['cdn_extensions']), "\r\n\t\0\x0B".' ;,');
 			$this->cdn_extensions = preg_split('/[|;,\s]+/', $this->cdn_extensions, NULL, PREG_SPLIT_NO_EMPTY);
 			$this->cdn_extensions = array_unique($this->cdn_extensions);
+
+			foreach($this->cdn_extensions as $_key => $_extension)
+				if(in_array($_extension, array('php'), TRUE))
+					unset($this->cdn_extensions[$_key]);
+			unset($_key, $_extension);
 		}
 
 		/**
@@ -68,51 +73,92 @@ namespace quick_cache // Root namespace.
 		abstract public function update_distro();
 
 		/**
-		 * @TODO Setup URL and content filters.
+		 * Setup URL and content filters. @TODO add content filters.
 		 */
 		public function setup_filters()
 		{
+			add_filter('home_url', array($this, 'url_filter'), PHP_INT_MAX - 10, 4);
+			add_filter('site_url', array($this, 'url_filter'), PHP_INT_MAX - 10, 4);
+
+			add_filter('network_home_url', array($this, 'url_filter'), PHP_INT_MAX - 10, 3);
+			add_filter('network_site_url', array($this, 'url_filter'), PHP_INT_MAX - 10, 3);
+
+			add_filter('content_url', array($this, 'url_filter'), PHP_INT_MAX - 10, 2);
+			add_filter('plugins_url', array($this, 'url_filter'), PHP_INT_MAX - 10, 2);
+		}
+
+		/**
+		 * Filter home/site URLs that should be served by the CDN.
+		 *
+		 * @param string       $url Input URL|URI|query; passed by filter.
+		 * @param string       $path The path component(s) passed through by the filter.
+		 * @param string|null  $scheme `NULL`, `http`, `https`, `login`, `login_post`, `admin`, or `relative`.
+		 * @param integer|null $blog_id Blog ID; passed only by non-`network_` filter variations.
+		 *
+		 * @return string The URL after having been filtered.
+		 */
+		public function url_filter($url, $path = '', $scheme = NULL, $blog_id = NULL)
+		{
+			return $this->filter_url($url, $scheme);
+		}
+
+		/**
+		 * Filter content for URLs that should be served by the CDN.
+		 *
+		 * @param string $string Input content string to filter; i.e. HTML code.
+		 *
+		 * @return string The content string after having been filtered.
+		 */
+		public function content_filter($string)
+		{
+			if(!$this->host) // Missing host name?
+				return $string; // Not possible.
+
+			if(!($string = (string)$string))
+				return $string; // Nothing to do here.
+
+			$regex = '/(["\'])(\/\/'.preg_quote($this->host, '/').'\/.+?)(\\1)/';
+			// @TODO Finish regex pattern(s). May need to look at relative paths also.
+			// @TODO Also need to do a better job of finding true URLs within certain contexts.
+
+			return preg_replace_callback($regex, array($this, '_content_filter_cb'), $string);
+		}
+
+		protected function _content_filter_cb(array $m)
+		{
+			return $m[1].$this->filter_url($m[2]).$m[3]; // @TODO Deal with HTML entities here.
 		}
 
 		/**
 		 * Filter URLs that should be served by the CDN.
 		 *
-		 * @param string      $url Input URL passed by one of several filters.
-		 * @param string|null $scheme Scheme passed by one of several filters.
+		 * @param string      $url_uri_query Input URL|URI|query.
+		 * @param string|null $scheme `NULL`, `http`, `https`, `login`, `login_post`, `admin`, or `relative`.
 		 *
 		 * @return string The URL after having been filtered.
 		 */
-		public function filter_url($url, $scheme = NULL)
+		protected function filter_url($url_uri_query, $scheme = NULL)
 		{
-			if(!($local_file = $this->local_file($url)))
-				return $url; // Not local.
+			if(!($local_file = $this->local_file($url_uri_query)))
+				return $url_uri_query; // Not local.
 
 			if(!in_array($local_file->extension, $this->cdn_extensions, TRUE))
-				return $url; // Not in the list of CDN extensions.
+				return $url_uri_query; // Not in the list of CDN extensions.
+
+			if(preg_match('/\/wp\-admin(?:[\/?#]|$)/i', $local_file->uri))
+				return $url_uri_query; // Exclude `wp-admin` URIs.
 
 			return set_url_scheme('//'.$this->cdn_host.$local_file->uri, $scheme);
 		}
 
-		/** @TODO Filter content for URLs that should be served by the CDN.
+		/**
+		 * Parse a URL|URI|query into a local file array.
 		 *
-		 * @param string $string
+		 * @param string $url_uri_query Input URL|URI|query.
 		 *
-		 * @return string The content string after having been filtered.
+		 * @return object|null An object with two properties: `uri` and `extension`.
+		 *    This returns NULL for any file that is not local, or not a file.
 		 */
-		public function filter_content($string)
-		{
-			if(!($string = (string)$string))
-				return $string; // Nothing to do here.
-
-			$regex = ''; // @TODO
-			return preg_replace_callback($regex, array($this, '_filter_content_cb'), $string);
-		}
-
-		protected function _filter_content_cb(array $m)
-		{
-			return $m[0]; // @TODO
-		}
-
 		protected function local_file($url_uri_query)
 		{
 			if(!$this->host) // Missing host name?
