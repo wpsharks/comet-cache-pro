cfb728e7677e25144c1db33b7aa1e53c8915705d
jaswsinc <jas@wsharks.com>
2015-05-02 18:27:35 -0800

    Remove unnecessary .htaccess.

--- src/includes/stub.php
+++ src/includes/stub.php
@@ -4,13 +4,12 @@
  *
  * @since 150422 Rewrite
  */
-namespace WebSharks\ZenCache\Pro {
+namespace WebSharks\ZenCache\Pro;
 
-    if (!defined('WPINC')) {
-        exit('Do NOT access this file directly: '.basename(__FILE__));
-    }
-    require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
-
-    const DIRECTORY = __DIR__; // Plugin file directory.
-    const GLOBAL_NS = 'zencache'; // Global namespace.
+if (!defined('WPINC')) {
+    exit('Do NOT access this file directly: '.basename(__FILE__));
 }
+require_once dirname(dirname(__FILE__)).'/vendor/autoload.php';
+
+const DIRECTORY = __DIR__; // Plugin file directory.
+const GLOBAL_NS = 'zencache'; // Global namespace.
