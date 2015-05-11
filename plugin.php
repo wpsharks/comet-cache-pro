246e69c71d07f66cb3a76170b3b36e4b40bff4b2
jaswsinc <jas@wsharks.com>
2015-05-11 07:38:46 -0800

    Lower min. requirement to PHP v5.3.2.

--- plugin.php
+++ plugin.php
@@ -14,7 +14,7 @@ Description: ZenCache is an advanced WordPress caching plugin inspired by simpli
 if (!defined('WPINC')) {
     exit('Do NOT access this file directly: '.basename(__FILE__));
 }
-$GLOBALS['wp_php_rv'] = '5.4.7'; //php-required-version//
+$GLOBALS['wp_php_rv'] = '5.3.2'; //php-required-version//
 if (require(dirname(__FILE__).'/src/vendor/websharks/wp-php-rv/src/includes/check.php')) {
     require_once dirname(__FILE__).'/src/includes/plugin.php';
 } else {
