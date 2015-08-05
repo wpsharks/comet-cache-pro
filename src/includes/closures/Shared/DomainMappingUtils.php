cecf81e2964d7e5a7f0a6cebdcab8d49f0c14360
jaswsinc <jas@wsharks.com>
2015-08-04 23:46:10 -0800

    Harden domain queries.

--- src/includes/closures/Shared/DomainMappingUtils.php
+++ src/includes/closures/Shared/DomainMappingUtils.php
@@ -212,7 +212,7 @@ $self->domainMappingBlogDomain = function ($blog_id = 0, $fallback = false) {
             $domain = $domains[0]; // Use the first of all possible domains.
         }
     } else { // A single primary domain in this case; i.e., `active` = primary.
-        $domain = (string) $wpdb->get_var('SELECT `domain` FROM `'.esc_sql($wpdb->base_prefix.'domain_mapping').'` WHERE `blog_id` = \''.esc_sql($blog_id).'\' AND `active` = \'1\' LIMIT 1');
+        $domain = (string) $wpdb->get_var('SELECT `domain` FROM `'.esc_sql($wpdb->base_prefix.'domain_mapping').'` WHERE `blog_id` = \''.esc_sql($blog_id).'\' AND `domain` IS NOT NULL AND `domain` != \'\' AND `active` = \'1\' LIMIT 1');
     }
     if (!$domain && $fallback && ($blog_details = $self->blogDetails($blog_id))) {
         $domain = $blog_details->domain; // Use original domain.
@@ -254,9 +254,9 @@ $self->domainMappingBlogDomains = function ($blog_id = 0) {
     $enforcing_primary_domain = !get_site_option('dm_no_primary_domain'); // Enforcing primary domain?
 
     if (!$enforcing_primary_domain) { // Not enforcing a primary domain, so let's pull all of the domains.
-        $domains = $wpdb->get_col('SELECT `domain` FROM `'.esc_sql($wpdb->base_prefix.'domain_mapping').'` WHERE `blog_id` = \''.esc_sql($blog_id).'\' ORDER BY `active` DESC');
+        $domains = $wpdb->get_col('SELECT `domain` FROM `'.esc_sql($wpdb->base_prefix.'domain_mapping').'` WHERE `blog_id` = \''.esc_sql($blog_id).'\' AND `domain` IS NOT NULL AND `domain` != \'\' ORDER BY `active` DESC');
     } else { // Primary domains in this case; i.e., `active` = primary.
-        $domains = $wpdb->get_col('SELECT `domain` FROM `'.esc_sql($wpdb->base_prefix.'domain_mapping').'` WHERE `blog_id` = \''.esc_sql($blog_id).'\' AND `active` = \'1\'');
+        $domains = $wpdb->get_col('SELECT `domain` FROM `'.esc_sql($wpdb->base_prefix.'domain_mapping').'` WHERE `blog_id` = \''.esc_sql($blog_id).'\' AND `domain` IS NOT NULL AND `domain` != \'\' AND `active` = \'1\'');
     }
     $wpdb->suppress_errors($suppressing_errors); // Restore.
 
