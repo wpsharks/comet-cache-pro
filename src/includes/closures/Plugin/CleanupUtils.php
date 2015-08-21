e767afb823c96e56aa37c611d9c0341707329978
jaswsinc <jas@wsharks.com>
2015-08-21 04:41:34 -0800

    Load average checks. See: websharks/zencache#347

--- src/includes/closures/Plugin/CleanupUtils.php
+++ src/includes/closures/Plugin/CleanupUtils.php
@@ -7,15 +7,19 @@ namespace WebSharks\ZenCache\Pro;
  * @since 15xxxx While working on directory stats.
  *
  * @attaches-to `'_cron_'.__GLOBAL_NS__.'_cleanup'`
- *
- * @TODO Disable automatically when load average is high.
- *  See: <https://github.com/websharks/zencache/issues/347>
- *  Note: this is impact the AdvancedCache expiration check also.
  */
 $self->cleanupCache = function () use ($self) {
     if (!$self->options['enable']) {
         return; // Nothing to do.
     }
+    /*[pro strip-from="lite"]*/
+    if ($self->options['cache_max_age_disable_if_load_average_is_gte'] && ($load_averages = $self->sysLoadAverages())) {
+        if (max($load_averages) >= $self->options['cache_max_age_disable_if_load_average_is_gte']) {
+            return; // Don't expire the cache when load average is high.
+        }
+    }
+    /*[/pro]*/
+
     /*[pro strip-from="lite"]*/
     if ($self->options['stats_enable']) {
         $dir_stats = DirStats::instance();
