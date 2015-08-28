a975205890ff765c8aa1c173b3975ae3bdc2a203
renzms <renz@websharks-inc.com>
2015-08-28 21:47:14 +0800

    Tweaks suggest by @jawsinc. see: websharks/zencache#502

--- src/includes/classes/MenuPageProUpdater.php
+++ src/includes/classes/MenuPageProUpdater.php
@@ -40,14 +40,16 @@ class MenuPageProUpdater extends MenuPage
         echo '      <a href="'.esc_attr('http://zencache.com/r/zencache-beta-testers-list/').'" target="_blank"><i class="si si-envelope"></i> '.__('Beta Testers', SLUG_TD).'</a>'."\n";
         echo '   </div>'."\n";
 	
-		echo '<div class="plugin-menu-page-version">'.
-			 '  <small><span>'.sprintf(__('%1$s&trade; v', SLUG_TD), esc_html(NAME)).'</span></small>'.
-			 ''.esc_html(VERSION).
-			 '   <a href="'.esc_attr('https://zencache.com/changelog/').'" target="_blank"> '.__('(changelog)', SLUG_TD).'</a>.'.
-			 '	<a href="'.esc_attr(add_query_arg(urlencode_deep(array('page' => GLOBAL_NS.'-pro-updater')), self_admin_url('/admin.php'))).'" target="_blank"> '.__('Latest Pro v', SLUG_TD).'</a>'.
-			 ''	 .$this->plugin->options['latest_pro_version'].
-			'</div>'."\n";
+		echo '<div class="plugin-menu-page-version">'."\n";
+		echo '  '.sprintf(__('%1$s&trade; v%2$s', SLUG_TD), esc_html(NAME), esc_html(VERSION))."\n";
 
+			if ($this->plugin->options['latest_pro_version'] && version_compare(VERSION, $this->plugin->options['latest_pro_version'], '<')) {
+				echo '(<a href="'.esc_attr(add_query_arg(urlencode_deep(array('page' => GLOBAL_NS.'-pro-updater')), self_admin_url('/admin.php'))).'" style="font-weight:bold;">'.__('update available', SLUG_TD).'</a>)'."\n";
+			} else {
+				echo '(<a href="'.esc_attr('https://zencache.com/changelog/').'" target="_blank">'.__('changelog', SLUG_TD).'</a>)'."\n";
+				}
+
+				echo '</div>'."\n";
 			
         echo '   <img src="'.$this->plugin->url('/src/client-s/images/pro-updater.png').'" alt="'.esc_attr(__('Pro Plugin Updater', SLUG_TD)).'" />'."\n";
 
