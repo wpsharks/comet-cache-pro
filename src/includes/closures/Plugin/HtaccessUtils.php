bcffefc32ded23f67dd863a65abc17a50e30a131
jaswsinc <jas@wsharks.com>
2015-10-22 13:02:35 -0800

    Nix last commit. Not necessary.

--- src/includes/closures/Plugin/HtaccessUtils.php
+++ src/includes/closures/Plugin/HtaccessUtils.php
@@ -39,8 +39,6 @@ $self->addWpHtaccess = function () use ($self) {
         } // ↑ Some files might be missing in the lite version.
 
         $_template_file_contents = trim(file_get_contents($_template_file));
-        $_template_file_contents = trim(preg_replace('/\/\*.*?\*\//s', '', $_template_file_contents));
-
         if (!($_template_file_contents = trim(file_get_contents($_template_file)))) {
             continue; // Template file empty; bypass.
         } // ↑ Some files might be empty in the lite version.
