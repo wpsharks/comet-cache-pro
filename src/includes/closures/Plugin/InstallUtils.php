639a7ad7cc7e7a9b7e8c1cb795293bb9d3453db4
Raam Dev <raam@websharks-inc.com>
2015-10-22 23:27:35 -0400

    Improve use of php_strip_whitespace()
    See websharks/zencache#591

--- src/includes/closures/Plugin/InstallUtils.php
+++ src/includes/closures/Plugin/InstallUtils.php
@@ -134,10 +134,13 @@ $self->addWpCacheToWpConfig = function () use ($self) {
     if (!is_readable($wp_config_file)) {
         return ''; // Not possible.
     }
-    if (!($wp_config_file_contents = php_strip_whitespace($wp_config_file))) {
+    if (!($wp_config_file_contents = file_get_contents($wp_config_file))) {
         return ''; // Failure; could not read file.
     }
-    if (preg_match('/\bdefine\s*\(\s*([\'"])WP_CACHE\\1\s*,\s*(?:\-?[1-9][0-9\.]*|TRUE|([\'"])(?:[^0\'"]|[^\'"]{2,})\\2)\s*\)\s*;/i', $wp_config_file_contents)) {
+    if (!($wp_config_file_contents_no_whitespace = php_strip_whitespace($wp_config_file))) {
+        return ''; // Failure; file empty
+    }
+    if (preg_match('/\bdefine\s*\(\s*([\'"])WP_CACHE\\1\s*,\s*(?:\-?[1-9][0-9\.]*|TRUE|([\'"])(?:[^0\'"]|[^\'"]{2,})\\2)\s*\)\s*;/i', $wp_config_file_contents_no_whitespace)) {
         return $wp_config_file_contents; // It's already in there; no need to modify this file.
     }
     if (!($wp_config_file_contents = $self->removeWpCacheFromWpConfig())) {
@@ -179,10 +182,13 @@ $self->removeWpCacheFromWpConfig = function () use ($self) {
     if (!($wp_config_file_contents = file_get_contents($wp_config_file))) {
         return ''; // Failure; could not read file.
     }
-    if (!preg_match('/([\'"])WP_CACHE\\1/i', $wp_config_file_contents)) {
+    if (!($wp_config_file_contents_no_whitespace = php_strip_whitespace($wp_config_file))) {
+        return ''; // Failure; file empty
+    }
+    if (!preg_match('/([\'"])WP_CACHE\\1/i', $wp_config_file_contents_no_whitespace)) {
         return $wp_config_file_contents; // Already gone.
     }
-    if (preg_match('/define\s*\(\s*([\'"])WP_CACHE\\1\s*,\s*(?:0|FALSE|NULL|([\'"])0?\\2)\s*\)\s*;/i', $wp_config_file_contents) && !is_writable($wp_config_file)) {
+    if (preg_match('/define\s*\(\s*([\'"])WP_CACHE\\1\s*,\s*(?:0|FALSE|NULL|([\'"])0?\\2)\s*\)\s*;/i', $wp_config_file_contents_no_whitespace) && !is_writable($wp_config_file)) {
         return $wp_config_file_contents; // It's already disabled, and since we can't write to this file let's let this slide.
     }
     if (!($wp_config_file_contents = preg_replace('/define\s*\(\s*([\'"])WP_CACHE\\1\s*,\s*(?:\-?[0-9\.]+|TRUE|FALSE|NULL|([\'"])[^\'"]*\\2)\s*\)\s*;/i', '', $wp_config_file_contents))) {
