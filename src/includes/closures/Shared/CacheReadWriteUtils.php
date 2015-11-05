ff14885a9385b5c4486df74a7d92469e40874717
jaswsinc <jas@wsharks.com>
2015-11-04 17:57:32 -0900

    @TODO `CacheReadWriteUtils.php`.

---
+++ src/includes/closures/Shared/CacheReadWriteUtils.php
@@ -0,0 +1,22 @@
+<?php
+namespace WebSharks\ZenCache\Pro;
+
+// @TODO
+
+/*
+ * Read cache file.
+ *
+ * @since 15xxxx Write to the cache.
+ */
+$self->cacheRead = function () use ($self) {
+    return;
+};
+
+/*
+ * Write cache file.
+ *
+ * @since 15xxxx Write to the cache.
+ */
+$self->cacheWrite = function () use ($self) {
+    return;
+};
