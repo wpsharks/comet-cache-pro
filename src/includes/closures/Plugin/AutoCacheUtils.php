ad3479b53487d4c7d178e43af40818d0e6dad8e9
Raam Dev <raam@websharks-inc.com>
2015-12-03 21:24:14 -0500

    Rename $__recursive to $is_nested_sitemap
    See websharks/zencache#616

--- src/includes/closures/Plugin/AutoCacheUtils.php
+++ src/includes/closures/Plugin/AutoCacheUtils.php
@@ -66,17 +66,17 @@ $self->autoCacheMaybeClearPrimaryXmlSitemapError = function ($force = false) use
  *
  * @since 15xxxx Improving XML Sitemap error checking.
  *
- * @param string    $sitemap       A URL to an XML sitemap file.
- *                                 This supports nested XML sitemap index files too; i.e. `<sitemapindex>`.
- *                                 Note that GZIP files are NOT supported at this time.
- * @param bool      $___recursive  Is this routine being called as part of a recursive routine?
- * @param bool|null $is_child_blog Is this routine being called from a child blog?
+ * @param string    $sitemap           A URL to an XML sitemap file.
+ *                                     This supports nested XML sitemap index files too; i.e. `<sitemapindex>`.
+ *                                     Note that GZIP files are NOT supported at this time.
+ * @param bool      $is_nested_sitemap Are we traversing a primary sitemap and now dealing with a nested sitemap?
+ * @param bool|null $is_child_blog     Is this routine being called from a child blog?
  *
  * @return bool `TRUE` if there was no failure fetching XML Sitemap, else `FALSE`. This also creates a dashboard notice in some cases.
  *
- * @note This routine is also used by the AutoCache class when the Auto-Cache Engine is running.
+ * @note  This routine is also used by the AutoCache class when the Auto-Cache Engine is running.
  */
-$self->autoCacheCheckXmlSitemap = function ($sitemap, $___recursive = false, $is_child_blog = null) use ($self) {
+$self->autoCacheCheckXmlSitemap = function ($sitemap, $is_nested_sitemap = false, $is_child_blog = null) use ($self) {
     $failure = ''; // Initialize.
 
     if (is_wp_error($head = wp_remote_head($sitemap, array('redirection' => 5)))) {
@@ -87,7 +87,7 @@ $self->autoCacheCheckXmlSitemap = function ($sitemap, $___recursive = false, $is
         $failure = sprintf(__('Content-Type (<code>%1$s</code>) indicates an error.', SLUG_TD), esc_html((string)@$head['headers']['content-type']));
     }
     if ($failure) { // Failure encountered above?
-        if (!$is_child_blog && !$___recursive && $self->options['auto_cache_sitemap_url']) { // If this is a primary sitemap location.
+        if (!$is_child_blog && !$is_nested_sitemap && $self->options['auto_cache_sitemap_url']) { // If this is a primary sitemap location.
             $self->dismissMainNotice('xml_sitemap_missing'); // Clear any previous XML Sitemap notice, which may reference an old URL; see http://wsharks.com/1SAofhP
             $self->enqueueMainNotice(
               sprintf(__('<strong>%1$s says...</strong> The Auto-Cache Engine is currently configured with an XML Sitemap location that could not be found. We suggest that you install the <a href="http://zencache.com/r/google-xml-sitemaps-plugin/" target="_blank">Google XML Sitemaps</a> plugin. Or, empty the XML Sitemap field and only use the list of URLs instead. See: <strong>Dashboard → %1$s → Auto-Cache Engine → XML Sitemap URL</strong>', SLUG_TD), esc_html(NAME)).'</p><hr />'.
@@ -98,7 +98,7 @@ $self->autoCacheCheckXmlSitemap = function ($sitemap, $___recursive = false, $is
         return false; // Nothing more we can do in this case.
     }
 
-    if (!$is_child_blog && !$___recursive) { // Any previous problems have been fixed; dismiss any existing failure notice
+    if (!$is_child_blog && !$is_nested_sitemap) { // Any previous problems have been fixed; dismiss any existing failure notice
         $self->dismissMainNotice('xml_sitemap_missing');
     }
 
