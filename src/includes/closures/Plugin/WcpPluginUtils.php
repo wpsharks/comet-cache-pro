6e5a0e642ea5e264ac2599807ba9558835cf1a9e
Raam Dev <raam@websharks-inc.com>
2015-12-05 20:08:07 -0500

    Set a $network_wide default in case parameter is not passed to hook
    See websharks/zencache#631

--- src/includes/closures/Plugin/WcpPluginUtils.php
+++ src/includes/closures/Plugin/WcpPluginUtils.php
@@ -10,11 +10,11 @@ namespace WebSharks\ZenCache\Pro;
 * @attaches-to `deactivated_plugin` hook.
 *
 * @param string $plugin Plugin basename.
-* @param bool True if activating|deactivating network-wide.
+* @param bool True if activating|deactivating network-wide. Defaults to boolean `FALSE` in case parameter is not passed to hook.
 *
 * @return int Total files wiped|cleared by this routine (if any).
 */
-$self->autoClearOnPluginActivationDeactivation = function ($plugin, $network_wide) use ($self) {
+$self->autoClearOnPluginActivationDeactivation = function ($plugin, $network_wide = false) use ($self) {
   if (!$self->applyWpFilters(GLOBAL_NS.'_auto_clear_on_plugin_activation_deactivation', true)) {
     return 0; // Nothing to do here.
   }
