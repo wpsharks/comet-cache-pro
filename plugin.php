68ee824a1a4468ea4581d8c938ca4a62d583f428
Raam Dev <raam@websharks-inc.com>
2016-01-18 20:52:52 -0500

    Use ${__FILE__}['apc_enabled'] instead of polluting global namespace
    See websharks/zencache.com#74

--- plugin.php
+++ plugin.php
@@ -5,15 +5,15 @@ if (!defined('WPINC')) {
 $GLOBALS['wp_php_rv'] = '5.3.2'; //php-required-version// // Leaving this at v5.3.2 so that we can have more control over Dashboard messages below.
 
 if (require(dirname(__FILE__).'/src/vendor/websharks/wp-php-rv/src/includes/check.php')) {
-    $_apc_enabled = (extension_loaded('apc') && filter_var(ini_get('apc.enabled'), FILTER_VALIDATE_BOOLEAN) && filter_var(ini_get('apc.cache_by_default'), FILTER_VALIDATE_BOOLEAN) && stripos((string)ini_get('apc.filters'), 'zencache') === false) ? true : false;
+    ${__FILE__}['apc_enabled'] = (extension_loaded('apc') && filter_var(ini_get('apc.enabled'), FILTER_VALIDATE_BOOLEAN) && filter_var(ini_get('apc.cache_by_default'), FILTER_VALIDATE_BOOLEAN) && stripos((string)ini_get('apc.filters'), 'zencache') === false) ? true : false;
 
-    if ((!version_compare(PHP_VERSION, '5.4', '>=') || $_apc_enabled)) {
+    if ((!version_compare(PHP_VERSION, '5.4', '>=') || ${__FILE__}['apc_enabled'])) {
 
         if (!version_compare(PHP_VERSION, '5.4', '>=') && is_admin()) {
             ${__FILE__}['php54_notice'] = '<h3 style="margin:.5em 0 .25em 0;">'.__('<strong>NOTICE: ZenCache Minimum PHP Version</strong></h3>', 'zencache');
             ${__FILE__}['php54_notice'] .= '<p style="margin-top:0;">'.sprintf(__('<strong>As of December 1st, 2015 ZenCache requires PHP 5.4 or higher.</strong> Your server is currently running PHP v%1$s. You will need to upgrade to PHP 5.4 or higher to run this version of ZenCache.', 'zencache'), esc_html(PHP_VERSION)).'</p>';
             ${__FILE__}['php54_notice'] .= '<p style="margin-top:0;">'.__('Learn more about this change here: <a href="http://zencache.com/r/new-minimum-php-version-php-5-4/" target="_blank">New Minimum PHP Version: PHP 5.4</a>', 'zencache').'</p>';
-            if ($_apc_enabled) {
+            if (${__FILE__}['apc_enabled']) {
                 ${__FILE__}['php54_notice'] .= '<p style="margin-top:0;">'.__('Your server is also running the <strong>outdated PHP APC extension</strong>. Please see: <a href="http://zencache.com/r/php-apc-extension-no-longer-supported/" target="_blank">PHP APC Extension No Longer Supported</a>', 'zencache').'</p>';
             }
 
@@ -31,10 +31,7 @@ if (require(dirname(__FILE__).'/src/vendor/websharks/wp-php-rv/src/includes/chec
                     '\';'
               )
             );
-            unset(${__FILE__}); // Housekeeping.
-        }
-
-        if ($_apc_enabled && is_admin()) {
+        } elseif (${__FILE__}['apc_enabled'] && is_admin()) {
             ${__FILE__}['apc_deprecated_notice'] = '<h3 style="margin:.5em 0 .25em 0;">'.__('<strong>NOTICE: ZenCache + PHP APC Extension</strong></h3>', 'zencache');
             ${__FILE__}['apc_deprecated_notice'] .= '<p style="margin-top:0;">'.sprintf(__('<strong>As of December 1st, 2015 ZenCache no longer runs with the outdated PHP APC extension.</strong> It appears that you\'re currently running PHP v%1$s with APC enabled. You will need to follow one of the actions below to run this version of ZenCache.', 'zencache'), esc_html(PHP_VERSION)).'</p>';
             ${__FILE__}['apc_deprecated_notice'] .= __('<h4 style="margin:0 0 .5em 0; font-size:1.25em;"><span class="dashicons dashicons-lightbulb"></span> Options Available (Action Required):</h4>', 'zencache');
@@ -58,7 +55,6 @@ if (require(dirname(__FILE__).'/src/vendor/websharks/wp-php-rv/src/includes/chec
                     '\';'
               )
             );
-            unset(${__FILE__}); // Housekeeping.
         }
     } else {
         require_once dirname(__FILE__).'/src/includes/plugin.php';
@@ -66,3 +62,5 @@ if (require(dirname(__FILE__).'/src/vendor/websharks/wp-php-rv/src/includes/chec
 } else {
     wp_php_rv_notice('ZenCache');
 }
+
+unset(${__FILE__}); // Housekeeping.
