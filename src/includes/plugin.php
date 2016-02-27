ce044161dfb2ac6555c239c1bab3307c41363f1a
Raam Dev <raam@websharks-inc.com>
2016-02-27 13:47:31 -0500

    Fix missing backwards compatibility for $GLOBALS['quick_cache']
    See websharks/comet-cache#691

--- src/includes/plugin.php
+++ src/includes/plugin.php
@@ -14,6 +14,7 @@ require_once dirname(__FILE__).'/stub.php';
 if (!Conflicts::check()) {
     $GLOBALS[GLOBAL_NS]  = new Plugin();
     $GLOBALS['zencache'] = $GLOBALS[GLOBAL_NS]; // Back compat.
+    $GLOBALS['quick_cache'] = $GLOBALS[GLOBAL_NS]; // Back compat.
 
     add_action('plugins_loaded', function() {
         require_once dirname(__FILE__).'/api.php';
