be95571b334393d5b066d610e1e48ce02266e4a6
Raam Dev <raam@websharks-inc.com>
2016-03-01 10:32:02 -0500

    Convert to Traits: Add `.build.php` script to generate Traits use-list
    See websharks/comet-cache#635

---
+++ src/includes/traits/Shared/.build.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Generate use-list for Trait files in current directory
+ *
+ * When run inside a directory called `Plugin/`, with a PHP file inside `Plugin/` called `ActionUtils.php`,
+ * this script will generate `use Traits\Plugin\ActionUtils`.
+ */
+if ($_handle = opendir(__DIR__)) {
+    while (false !== ($_file = readdir($_handle))) {
+        if ($_file != '.' && $_file != '..' && $_file != '.build.php' && stristr($_file, '.php') !== false) {
+            echo 'use Traits\\'.basename(__DIR__).'\\'.basename($_file, '.php').';'.PHP_EOL;
+        }
+    }
+    closedir($_handle);
+    unset($_file, $_files);
+}
