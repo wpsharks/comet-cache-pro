eae15d90257cc08b4993271dc1d0ae3759854713
jaswsinc <jaswsinc@wsharks.com>
2016-04-06 14:11:55 -0800

    Build test.

--- tests/includes/phpunit.php
+++ tests/includes/phpunit.php
@@ -4,6 +4,16 @@
  */
 namespace WebSharks\CometCache\Pro;
 
+$handler = function ($errorNumber, $errorString, $errorFile, $errorLine) {
+        echo "
+ERROR INFO
+Message: $errorString
+File: $errorFile
+Line: $errorLine
+";
+};
+set_error_handler($handler);
+
 require_once $_SERVER['CI_NFO_WWW_DIR'].'/wp-load.php';
 
 $GLOBALS[GLOBAL_NS]->addWpHtaccess();
