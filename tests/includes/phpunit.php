61c160a368c29742158b1172301de0fd19aafd77
jaswsinc <jaswsinc@wsharks.com>
2016-04-06 15:05:36 -0800

    Remove debuggers.

--- tests/includes/phpunit.php
+++ tests/includes/phpunit.php
@@ -4,16 +4,6 @@
  */
 namespace WebSharks\CometCache\Pro;
 
-$handler = function ($errorNumber, $errorString, $errorFile, $errorLine) {
-        echo "
-ERROR INFO
-Message: $errorString
-File: $errorFile
-Line: $errorLine
-";
-};
-set_error_handler($handler);
-
 require_once $_SERVER['CI_NFO_PHPUNIT_BOOTSTRAP'];
 require_once $_SERVER['CI_NFO_WWW_DIR'].'/wp-load.php';
 
