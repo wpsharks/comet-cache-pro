dc71a46df990f18fe1193ec59364bf57da53957f
jaswsinc <jaswsinc@wsharks.com>
2016-04-06 12:44:29 -0800

    Build test.

--- tests/includes/phpunit.php
+++ tests/includes/phpunit.php
@@ -4,13 +4,7 @@
  */
 namespace WebSharks\CometCache\Pro;
 
-error_reporting(-1);
-ini_set('display_errors', true);
-
 require_once $_SERVER['CI_NFO_WWW_DIR'].'/wp-load.php';
-
-error_reporting(-1);
-ini_set('display_errors', true);
 
 $GLOBALS[GLOBAL_NS]->addWpHtaccess();
 $GLOBALS[GLOBAL_NS]->addWpCacheToWpConfig();
