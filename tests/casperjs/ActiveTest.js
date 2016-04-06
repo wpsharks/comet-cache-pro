67c07504a544f501f711daa0c9fb2e57259a9b57
jaswsinc <jaswsinc@wsharks.com>
2016-04-06 08:43:14 -0800

    Debug ActiveTest.js.

--- tests/casperjs/ActiveTest.js
+++ tests/casperjs/ActiveTest.js
@@ -6,11 +6,11 @@ $.test.begin('Check if plugin is active.', function () {
     $.thenOpen($$.www.url('/wp-admin/network/admin.php?page=' + $$$.GLOBAL_NS));
   } else {
     $.thenOpen($$.www.url('/wp-admin/admin.php?page=' + $$$.GLOBAL_NS), function() {
-    $.echo($.getHTML());
+    this.echo(this.getHTML());
   });
   }
   $.then(function () {
-    $.echo($.getHTML());
+    this.echo(this.getHTML());
     $.test.assertExists('input[type="radio"][name="' + $$$.GLOBAL_NS + '[saveOptions][enable]"][value="1"]:checked');
   });
   $$.wp.thenLogout();
