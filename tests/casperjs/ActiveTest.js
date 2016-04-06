28a74193b65abaa4be134746c4749974a8dd9aec
jaswsinc <jaswsinc@wsharks.com>
2016-04-06 08:24:40 -0800

    Dump HTML for debugging.

--- tests/casperjs/ActiveTest.js
+++ tests/casperjs/ActiveTest.js
@@ -8,6 +8,7 @@ $.test.begin('Check if plugin is active.', function () {
     $.thenOpen($$.www.url('/wp-admin/admin.php?page=' + $$$.GLOBAL_NS));
   }
   $.then(function () {
+    $.echo($.getHTML());
     $.test.assertExists('input[type="radio"][name="' + $$$.GLOBAL_NS + '[saveOptions][enable]"][value="1"]:checked');
   });
   $$.wp.thenLogout();
