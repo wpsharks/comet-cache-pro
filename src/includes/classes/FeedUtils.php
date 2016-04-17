1e7e68648fb50936e1bbb8029d1eaa83f2ecc0bb
Raam Dev <raam@websharks-inc.com>
2016-04-17 16:06:43 -0400

    Fix fatal error Undefined class constant ‘CACHE_PATH_NO_SCHEME’
    See websharks/comet-cache#739

--- src/includes/classes/FeedUtils.php
+++ src/includes/classes/FeedUtils.php
@@ -1,12 +1,14 @@
 <?php
 namespace WebSharks\CometCache\Pro\Classes;
 
+use WebSharks\CometCache\Pro\Interfaces;
+
 /**
  * Feed Utils.
  *
  * @since 150422 Rewrite.
  */
-class FeedUtils extends AbsBase
+class FeedUtils extends AbsBase implements Interfaces\Shared\CachePathConsts
 {
     /**
      * @type string WordPress `home_url()`.
