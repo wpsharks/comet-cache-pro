93d37f7d0c2cb6148759e93ba386408fd63ad2ed
Raam Dev <raam@websharks-inc.com>
2016-05-17 14:43:49 -0400

    Add support for data-toggle in Menu Pages
    See websharks/comet-cache#768

--- src/client-s/js/menu-pages.js
+++ src/client-s/js/menu-pages.js
@@ -22,7 +22,7 @@
     $('[data-action]', plugin.$menuPage).on('click', plugin.doDataAction);
     $('[data-toggle-target]', plugin.$menuPage).on('click', plugin.doDataToggleTarget);
 
-    $('select[name$="_enable\\]"]', plugin.$menuPage).not('.-no-if-enabled').on('change', plugin.enableDisable).trigger('change');
+    $('select[name$="_enable\\]"], select[data-toggle~="enable-disable"]', plugin.$menuPage).not('.-no-if-enabled').on('change', plugin.enableDisable).trigger('change');
 
     /*![pro strip-from='lite']*/
     $('textarea[name$="\[cdn_hosts\]"]', plugin.$menuPage).on('input propertychange', plugin.handleCdnHostsChange);
