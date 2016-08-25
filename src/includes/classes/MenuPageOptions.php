e9ab7142b89ddaf009dae4a4ebcfc60e556f7bdb
jaswsinc <jaswsinc@wsharks.com>
2016-08-24 18:39:19 -0800

    Update Credentials moves to Basic Options.

--- src/includes/classes/MenuPageOptions.php
+++ src/includes/classes/MenuPageOptions.php
@@ -237,9 +237,9 @@ class MenuPageOptions extends MenuPage
         echo '      <p>'.sprintf(__('%1$s Pro is a premium product available for purchase @ <a href="http://cometcache.com/prices/" target="_blank">cometcache.com</a>. In order to connect with our update servers, you must supply your License Key. Your License Key is located under "My Account" when you log in @ <a href="http://cometcache.com/" target="_blank">cometcache.com</a>. This will authenticate your copy of %1$s Pro; providing you with access to the latest version. You only need to enter these credentials once. %1$s Pro will save them in your WordPress database; making future upgrades even easier. <i class="si si-smile-o"></i> If you prefer to upgrade manually, see <a href="https://cometcache.com/r/kb-article-how-to-manually-upgrade-comet-cache-pro/">this article</a>.', SLUG_TD), esc_html(NAME)).'</p>'."\n";
         echo '      <hr />'."\n";
         echo '      <h3>'.sprintf(__('Username', SLUG_TD), esc_html(NAME)).'</h3>'."\n";
-        echo '      <p><input type="text" name="'.esc_attr(GLOBAL_NS).'[saveOptions][username]" value="'.esc_attr($this->plugin->options['pro_update_username']).'" autocomplete="new-password" /></p>'."\n";
+        echo '      <p><input type="text" name="'.esc_attr(GLOBAL_NS).'[saveOptions][pro_update_username]" value="'.esc_attr($this->plugin->options['pro_update_username']).'" autocomplete="new-password" /></p>'."\n";
         echo '      <h3>'.sprintf(__('License Key', SLUG_TD), esc_html(NAME)).'</h3>'."\n";
-        echo '      <p><input type="password" name="'.esc_attr(GLOBAL_NS).'[saveOptions][password]" value="'.esc_attr($this->plugin->options['pro_update_password']).'" autocomplete="new-password" /></p>'."\n";
+        echo '      <p><input type="password" name="'.esc_attr(GLOBAL_NS).'[saveOptions][pro_update_password]" value="'.esc_attr($this->plugin->options['pro_update_password']).'" autocomplete="new-password" /></p>'."\n";
         echo '   </div>'."\n";
 
         echo '</div>'."\n";
