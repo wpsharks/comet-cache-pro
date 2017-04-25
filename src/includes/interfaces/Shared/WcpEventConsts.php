6052230c168cabcb8ed86f2f8f47668f7a421132
jaswrks <jas@src.works>
2017-04-25 13:45:46 -0800

    Drop-in for event constants to come soon. See: https://github.com/websharks/comet-cache/issues/869

---
+++ src/includes/interfaces/Shared/WcpEventConsts.php
@@ -0,0 +1,16 @@
+<?php
+namespace WebSharks\CometCache\Pro\Interfaces\Shared;
+
+interface WcpEventConsts
+{
+    /**
+     * WCP event/reason constant.
+     *
+     * @since 17xxxx First documented version.
+     *
+     * @type string A unique string identifier in the set of `WCP_EVENT_` constants.
+     */
+    const WCP_EVENT_POST_UPDATED = 'wcp_event_post_updated';
+
+    // @TODO See <https://github.com/websharks/comet-cache/issues/869>
+}
