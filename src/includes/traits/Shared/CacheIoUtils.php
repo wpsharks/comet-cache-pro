62a401c03d99d8f1693b2b0d86fdc4b143cbf14c
jaswrks <jas@src.works>
2017-04-25 14:53:41 -0800

    Beginning work on `CacheIoUtils{}`

---
+++ src/includes/traits/Shared/CacheIoUtils.php
@@ -0,0 +1,31 @@
+<?php
+namespace WebSharks\CometCache\Pro\Traits\Shared;
+
+use WebSharks\CometCache\Pro\Classes;
+
+trait CacheIoUtils
+{
+    /**
+     * Cache read.
+     *
+     * @since 17xxxx IO utils.
+     *
+     * @return string|null `null` if nonexistent.
+     */
+    public function cacheRead()
+    {
+        return null; // @TODO
+    }
+
+    /**
+     * Cache write.
+     *
+     * @since 17xxxx IO utils.
+     *
+     * @return bool True on success.
+     */
+    public function cacheWrite()
+    {
+        return false; // @TODO
+    }
+}
