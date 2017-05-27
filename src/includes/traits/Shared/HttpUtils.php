9c27afa63d4f8a5bb2f7373101ac4d9b57e08a95
Raam Dev <raam@websharks-inc.com>
2017-05-27 14:00:58 -0400

    Allow cachable HTTP headers list to be filtered
    See websharks/comet-cache#892

--- src/includes/traits/Shared/HttpUtils.php
+++ src/includes/traits/Shared/HttpUtils.php
@@ -124,7 +124,7 @@ trait HttpUtils
         if (($status = (string) $this->httpStatus())) {
             array_unshift($headers, $this->httpProtocol().' '.$status);
         }
-        return $headers;
+        return apply_filters(GLOBAL_NS.'_cacheable_headers', $headers);
     }
 
     /**
